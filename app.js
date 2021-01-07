10ae9e3458544c747bf5db432463d8bfba6fc3cc
codewithchu <cwc@arthurchu.ca>
2021-01-07 11:57:37 -0500

    09-cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 require('dotenv').config(); //dotenv
 require('./config/db'); //database connection
 
 //MIDDLEWARES
+app.use(cors());
+
 //Parsing Body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
